8f5b8529ddc09bb9eb4baff7d0518348f9097083
초시맥북 <fyg7575@gmail.com>
2026-02-19 04:18:17 +0900

    feat: ReadStatusController 문서 작성

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -1,7 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.readstatus.*;
+import com.sprint.mission.discodeit.dto.user.UserResponseDto;
 import com.sprint.mission.discodeit.service.ReadStatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +26,34 @@ import java.util.UUID;
 public class ReadStatusController {
     private final ReadStatusService readStatusService;
 
+    @Operation(summary = "메시지 확인 여부 객체 생성")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "객체 생성 성공",
+                    content = @Content(
+                            schema = @Schema(implementation = ReadStatusResponseDto.class)
+    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 유저를 찾을 수 없음",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = "User with username {username} not found"
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 채널을 찾을 수 없음",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = "Channel with {channelid} not found"
+                            )
+                    )
+            )
+    })
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<ReadStatusResponseDto> create_1(
             @RequestBody ReadStatusCreateRequestDto dto
@@ -26,6 +61,25 @@ public class ReadStatusController {
         return ResponseEntity.status(201).body(readStatusService.create(dto));
     }
 
+    @Operation(summary = "메시지 확인 여부 갱신")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "메시지 확인 여부 갱신 성공",
+                    content = @Content(
+                            schema = @Schema(implementation = ReadStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 객체를 찾을 수 없음",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = "ReadStatus with {readStatusId} not found"
+                            )
+                    )
+            )
+    })
     @RequestMapping(value = "/{readStatusId}", method = RequestMethod.PATCH)
     public ResponseEntity<ReadStatusResponseDto> update_1(
             @PathVariable UUID readStatusId,
@@ -34,6 +88,25 @@ public class ReadStatusController {
         return ResponseEntity.ok(readStatusService.update(readStatusId, requestDto));
     }
 
+    @Operation(summary = "메시지 확인 여부 가져오기")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "메시지 확인 여부 갱신 성공",
+                    content = @Content(
+                            schema = @Schema(implementation = ReadStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 객체를 찾을 수 없음",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = "ReadStatus with {readStatusId} not found"
+                            )
+                    )
+            )
+    })
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<IsMessageReadResponseDto>> getReadStatus(
             @RequestParam UUID userId
