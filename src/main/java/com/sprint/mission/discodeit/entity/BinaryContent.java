1f134354dce63ba5fa055ff452d42ca16662eb78
초시맥북 <fyg7575@gmail.com>
2026-03-04 02:46:54 +0900

    fix: BinaryContent 엔티티 데이터베이스에 컬럼 등록

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,17 +1,38 @@
 package com.sprint.mission.discodeit.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.UUID;
 
+@NoArgsConstructor
+@Setter
 @Getter
+@Entity(name = "binary_contents")
 public class BinaryContent extends BaseEntity{
-    private final String contentType;
-    private final byte[] bytes;
-    private final Long size;
-    private final String fileName;
+    @NotNull
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
+    private String contentType;
+
+    @NotNull
+    @Column(nullable = false)
+    private byte[] bytes;
+
+    @NotNull
+    @Column(nullable = false)
+    private Long size;
+
+    @NotNull
+    @Column(nullable = false)
+    private String fileName;
 
     public BinaryContent(byte[] bytes, String contentType, String fileName, Long size){
         super();
