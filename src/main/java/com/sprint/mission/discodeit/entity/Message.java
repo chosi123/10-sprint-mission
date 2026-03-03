94d69ae6f797b62edcb29d4bd8e2977c979a310b
초시맥북 <fyg7575@gmail.com>
2026-03-04 03:41:49 +0900

    fix: Message 엔티티 DB에 테이블 등록 (첨부파일 관련은 일단 보류)

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,18 +1,33 @@
 package com.sprint.mission.discodeit.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
+@NoArgsConstructor
+@Entity(name = "messages")
 @Getter
 public class Message extends BaseUpdatableEntity{
+    @Column
     private String content;
     //
-    private UUID channelId;
-    private UUID authorId;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "channel_id", nullable = false)
+    private Channel channel;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
     private List<UUID> attachmentIds;
 
     public Message(String content, UUID channelId, UUID authorId, List<UUID> attachments) {
