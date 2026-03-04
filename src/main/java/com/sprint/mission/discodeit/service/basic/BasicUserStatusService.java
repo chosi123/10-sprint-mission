d613692a1903345756b426cc45013f7d58fff87a
초시맥북 <fyg7575@gmail.com>
2026-03-04 17:55:55 +0900

    fix: UserStatusService에 트랜잭션 적용 및 엔티티 구조 변경에 대응

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
@@ -3,12 +3,14 @@ package com.sprint.mission.discodeit.service.basic;
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusCreateRequestDto;
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusResponseDto;
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusUpdateRequestDto;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.exception.UserNotFoundException;
 import com.sprint.mission.discodeit.mapper.userstatus.UserStatusResponseMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserStatusService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,19 +26,17 @@ public class BasicUserStatusService implements UserStatusService {
     private final UserStatusResponseMapper userStatusResponseMapper;
 
     @Override
+    @Transactional
     public UserStatusResponseDto create(UserStatusCreateRequestDto userStatusCreateRequestDto) {
-        if(!userRepository.existsById(userStatusCreateRequestDto.userId())) throw new UserNotFoundException(userStatusCreateRequestDto.userId());
-        if(userStatusRepository.findAll()
-                .stream()
-                .anyMatch(userStatus ->
-                        userStatus.getUserId().equals(userStatusCreateRequestDto.userId())))
-            throw new AssertionError("UserStatus already exists");
+        User user = userRepository.findById(userStatusCreateRequestDto.userId())
+                .orElseThrow(() -> new UserNotFoundException(userStatusCreateRequestDto.userId()));
 
-        UserStatus userstatus = userStatusRepository.save(new UserStatus(userStatusCreateRequestDto.userId()));
+        UserStatus userstatus = userStatusRepository.save(new UserStatus(user));
         return userStatusResponseMapper.toDto(userstatus);
     }
 
     @Override
+    @Transactional
     public UserStatusResponseDto find(UUID id) {
         UserStatus userStatus = userStatusRepository.findById(id)
                 .orElseThrow(() -> new AssertionError("UserStatus not found"));
@@ -44,6 +44,7 @@ public class BasicUserStatusService implements UserStatusService {
     }
 
     @Override
+    @Transactional
     public List<UserStatusResponseDto> findAll(UUID id) {
         return userStatusRepository.findAll().stream()
                 .map(userStatusResponseMapper::toDto)
@@ -51,6 +52,7 @@ public class BasicUserStatusService implements UserStatusService {
     }
 
     @Override
+    @Transactional
     public UserStatusResponseDto update(UUID id, UserStatusUpdateRequestDto userStatusUpdateRequestDto) {
         UserStatus userStatus = userStatusRepository.findById(id)
                 .orElseThrow(() -> new AssertionError("UserStatus not found"));
@@ -61,6 +63,8 @@ public class BasicUserStatusService implements UserStatusService {
         return userStatusResponseMapper.toDto(userStatus);
     }
 
+    @Override
+    @Transactional
     public UserStatusResponseDto updateByUserId(UUID id, UserStatusUpdateRequestDto userStatusUpdateRequestDto){
         UserStatus userStatus = userStatusRepository.findByUserId(id)
                         .orElseThrow(() -> new AssertionError("UserStatus not found"));
@@ -72,6 +76,7 @@ public class BasicUserStatusService implements UserStatusService {
         return userStatusResponseMapper.toDto(userStatus);
     }
 
+    @Transactional
     @Override
     public void delete(UUID id) {
         userStatusRepository.deleteById(id);
