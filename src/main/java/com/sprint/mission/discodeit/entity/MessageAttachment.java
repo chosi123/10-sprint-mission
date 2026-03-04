1816397c0e92d79f95e49d845bcae06e1a3da11f
초시맥북 <fyg7575@gmail.com>
2026-03-04 10:46:18 +0900

    fix: MessageAttachment 복합키 클래스 추가, 컬럼 설정 추가

--- src/main/java/com/sprint/mission/discodeit/entity/MessageAttachment.java
+++ src/main/java/com/sprint/mission/discodeit/entity/MessageAttachment.java
@@ -1,24 +1,28 @@
 package com.sprint.mission.discodeit.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "message_attachments")
 @NoArgsConstructor
 @Getter
 @Setter
-public class MessageAttachments {
+public class MessageAttachment {
     //엔티티 구조도에는 없으나, 키값은 필수이므로 임의로 작성함.
-    @Id
-    @GeneratedValue
-    private Long id;
+    @EmbeddedId
+    private MessageAttachmentId id;
 
     @ManyToOne
+    @NotNull
+    @JoinColumn(name = "message_id")
     private Message message;
 
     @OneToOne
+    @NotNull
+    @JoinColumn(name = "attachment_id", unique = true)
     private BinaryContent binaryContent;
 
 }
