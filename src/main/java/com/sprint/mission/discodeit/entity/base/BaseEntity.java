8cfb495593ebbb205cb6404405c129d78299aca2
초시맥북 <fyg7575@gmail.com>
2026-03-05 10:12:42 +0900

    fix: id에 @GeneratedValue 적용, 생성자를 통한 아이디 생성 로직 삭제, baseEntity, BaseUpdatableEntity 클래스에 @EntityListeners(AuditingEntityListener.class) 추가

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -1,28 +1,24 @@
 package com.sprint.mission.discodeit.entity.base;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public abstract class BaseEntity {
     @Id
+    @GeneratedValue
     protected UUID id;
 
     @CreatedDate
     @NotNull
     @Column(nullable = false, updatable = false)
     protected Instant createdAt;
-
-    protected BaseEntity() {
-        this.id = UUID.randomUUID();
-    }
 }
