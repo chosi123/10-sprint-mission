96d770d7fe858529ae2bbcb7cacf115deb3f91c8
초시맥북 <fyg7575@gmail.com>
2026-03-05 16:26:10 +0900

    fix: MessageResponseDto와 대응 매퍼 설정

--- src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
@@ -1,16 +1,19 @@
 package com.sprint.mission.discodeit.dto.message;
 
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponseDto;
+import com.sprint.mission.discodeit.dto.user.UserResponseDto;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public record MessageResponseDto(
+        UUID id,
         Instant createdAt,
         Instant updatedAt,
-        UUID id,
         String content,
         UUID channelId,
-        UUID authorId,
-        List<UUID> attachmentIds
+        UserResponseDto author,
+        List<BinaryContentResponseDto> attachments
 ) {
 }
--- src/main/java/com/sprint/mission/discodeit/mapper/message/MessageResponseMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/message/MessageResponseMapper.java
@@ -1,10 +1,24 @@
 package com.sprint.mission.discodeit.mapper.message;
 
 import com.sprint.mission.discodeit.dto.message.MessageResponseDto;
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.MessageAttachment;
+import com.sprint.mission.discodeit.mapper.binarycontent.BinaryContentResponseMapper;
+import com.sprint.mission.discodeit.mapper.user.UserResponseMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {BinaryContentResponseMapper.class, UserResponseMapper.class})
 public interface MessageResponseMapper {
+    @Mapping(target = "channelId", source = "channel.id")
+    @Mapping(target = "attachments", source = "attachments")
     MessageResponseDto toDto(Message message);
+
+    default BinaryContent map(MessageAttachment attachment){
+        return attachment.getBinaryContent();
+    }
 }
