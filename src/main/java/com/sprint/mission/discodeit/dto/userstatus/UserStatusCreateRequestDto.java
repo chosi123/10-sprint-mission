862cf704a541b6a754c74bcf1c54545c6aaae54a
초시맥북 <fyg7575@gmail.com>
2026-03-27 17:48:42 +0900

    fix: 요청 dto에 유효성 검증 추가

--- src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusCreateRequestDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusCreateRequestDto.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.dto.userstatus;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record UserStatusCreateRequestDto(
+        @NotBlank
         UUID userId
 ) {
 }
