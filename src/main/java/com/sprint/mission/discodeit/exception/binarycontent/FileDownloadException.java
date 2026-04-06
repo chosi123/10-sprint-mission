d89cdc4a4dea5e9440dd39461e22adc7e3cc2024
초시맥북 <fyg7575@gmail.com>
2026-04-06 16:09:24 +0900

    refactor: 사용되지 않는 파일 정리

--- src/main/java/com/sprint/mission/discodeit/exception/binarycontent/FileDownloadException.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.exception.binarycontent;
-
-import com.sprint.mission.discodeit.exception.ErrorCode;
-
-public class FileDownloadException extends BinaryContentException{
-    public FileDownloadException(){
-        super(ErrorCode.DOWNLOAD_FAILED);
-    }
-}
