74326dfac670bf184291418f5ca07bbb63bfb2d3
초시맥북 <fyg7575@gmail.com>
2026-04-07 18:13:40 +0900

    refactor: AWS S3과 연동하여 첨부파일을 업로드/다운로드하는 로직 작성

--- src/main/java/com/sprint/mission/discodeit/config/AwsProperties.java
+++ src/main/java/com/sprint/mission/discodeit/config/AwsProperties.java
@@ -8,24 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 @Getter
 @Configuration
-@ConfigurationProperties(prefix = "aws")
+@ConfigurationProperties(prefix = "discodeit.storage.s3")
 public class AwsProperties {
-    private Credentials credentials = new Credentials();
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
     private String region;
-    private S3 s3 = new S3();
-
-    @Setter
-    @Getter
-    public static class Credentials {
-        // getter/setter
-        private String accessKey;  // env에서 주입
-        private String secretKey;  // env에서 주입
-    }
-
-    @Setter
-    @Getter
-    public static class S3 {
-        private String bucket;
-    }
-
+    private Long presignedUrlExpiration;
 }
