cfd2031f8f547e087c1facdbbe1ad8261829b27b
초시맥북 <fyg7575@gmail.com>
2026-05-14 12:41:27 +0900

    feat: 로그인 폼 설정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -15,6 +15,10 @@ public class SecurityConfig {
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+
+        )
+        .formLogin(login -> login
+            .loginProcessingUrl("/api/auth/login")
         );
 
 
