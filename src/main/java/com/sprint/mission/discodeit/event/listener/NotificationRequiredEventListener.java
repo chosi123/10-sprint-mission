16f1d81e0a6307c35a20507bdf14da78606d8976
초시맥북 <fyg7575@gmail.com>
2026-06-08 16:15:14 +0900

    feat: kafka 이벤트 발행 로직 작성, 기존 이벤트 로직 비활성

--- src/main/java/com/sprint/mission/discodeit/event/listener/NotificationRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/NotificationRequiredEventListener.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class NotificationRequiredEventListener {
 
