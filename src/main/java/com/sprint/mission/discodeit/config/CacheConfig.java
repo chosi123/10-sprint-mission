61284bb962f61485f9585f072925d48cf5b3e190
초시맥북 <fyg7575@gmail.com>
2026-06-09 10:16:21 +0900

    feat: redis 적용

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,31 +1,64 @@
 package com.sprint.mission.discodeit.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import java.time.Duration;
+import java.util.Set;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class CacheConfig {
 
   @Bean
-  public CacheManager cacheManager() {
-    CaffeineCacheManager cacheManager = new CaffeineCacheManager(
-        "userChannels",
-        "userNotifications",
-        "users"
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(
+        "redis",
+        6379
+    );
+  }
+
+  @Bean
+  public CacheManager cacheManager(
+      RedisConnectionFactory connectionFactory,
+      ObjectMapper objectMapper
+  ) {
+    ObjectMapper redisObjectMapper = objectMapper.copy();
+    redisObjectMapper.activateDefaultTyping(
+        LaissezFaireSubTypeValidator.instance,
+        ObjectMapper.DefaultTyping.EVERYTHING,
+        JsonTypeInfo.As.PROPERTY
     );
 
-    cacheManager.setCaffeine(
-        Caffeine.newBuilder()
-            .maximumSize(1000)
-            .expireAfterWrite(Duration.ofMinutes(10))
-            .recordStats()
-    );
+    RedisSerializer<Object> serializer =
+        new GenericJackson2JsonRedisSerializer(redisObjectMapper);
 
-    return cacheManager;
+    RedisCacheConfiguration configuration =
+        RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+            );
+
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(configuration)
+        .initialCacheNames(Set.of(
+            "userChannels",
+            "userNotifications",
+            "users"
+        ))
+        .build();
   }
 
 }
