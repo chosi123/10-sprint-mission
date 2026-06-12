b97e49caea8449535c47ed72128ad72d0b314465
초시맥북 <fyg7575@gmail.com>
2026-06-12 17:31:17 +0900

    feat: 프론트엔드 코드 3.0.0 업데이트

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,11 +1,20 @@
 package com.sprint.mission.discodeit.config;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/sub");
+    config.setApplicationDestinationPrefixes("/pub");
+
+  }
 
 }
