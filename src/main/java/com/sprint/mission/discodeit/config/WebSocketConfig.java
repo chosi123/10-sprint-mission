3f8f8c75b59df3daadd99deb040104fb3e116a66
초시맥북 <fyg7575@gmail.com>
2026-06-12 17:35:15 +0900

    feat: stomp 엔드포인트 설정 추가

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -14,6 +15,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
     config.enableSimpleBroker("/sub");
     config.setApplicationDestinationPrefixes("/pub");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry){
+    registry.addEndpoint("/ws").withSockJS();
 
   }
 
